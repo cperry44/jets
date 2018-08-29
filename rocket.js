464c06858429672a62e0074f5b0cc8019fb8eff0
Tien Linda Vu <lindavu@Tiens-MacBook-Pro.local>
2018-08-29 14:40:28 -0400

    more engines vroom

--- rocket.js
+++ rocket.js
@@ -1,9 +1,12 @@
 var Rocket = function (color, size) {
- this.color = color;
- this.size = size;
+  this.color = color;
+  this.size = size;
 
- this.launch = function () {
-   console.log('3, 2, 1, blastoff!');
+  this.launch = function () {
+    console.log('3, 2, 1, blastoff!');
+  }
+  this.engageSecondaryEngines = function() {
+    console.log('Secondary Engines Engaged!');
+  }
 }
-
 module.exports = Rocket
